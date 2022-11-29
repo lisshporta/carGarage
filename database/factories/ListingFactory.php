f0e1a35aed97c69ea516d795850666201091c57c
Lis Shporta <lis.shporta@gmail.com>
2022-11-29 10:23:12 +0100

    Database Setup

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'brand' => $this->faker->name(),
+            'name' => $this->faker->name(),
+            'production_year' => $this->faker->randomDigit(),
+            'mileage' => $this->faker->randomDigit(),
+            'fuel_type' => $this->faker->word(),
+            'transmission' => $this->faker->word()
+        ];
+    }
+}
