1166d2b293a5156d4243cc7850a9c9c6426bbef0
Lis Shporta <lis.shporta@gmail.com>
2023-01-15 00:16:36 +0100

    Custom Page for Maintenance Mode

---
+++ resources/views/maintenance-mode.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style> 
+    body { background-color: #f0f0f0;}
+     .center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+     </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script rel="preconnect" src="https://cdn.tailwindcss.com"></script>
+    <title>Under Maintenance</title>
+</head>
+<body>
+
+    <section class="pt-5">
+        <div class="text-center pt-5">
+        <img class="h-96 w-96 center" src="images/logo.png" >
+            <h1 class=" text-5xl font-bold pt-3">
+                Under Maintenance 
+            </h1>
+            <hr>
+            <p class="mt-5 font-semibold">Our Website is currently undergoing scheduled maintenance.
+                <br> We Should be back shortly.
+            </p>
+        </div>
+    </section>
+
+</body>
+</html>
