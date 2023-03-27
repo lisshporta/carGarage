ad66eabd115137ae0c6ffe0eade09022999411ff
lisshporta <lis.shporta@gmail.com>
2023-03-27 20:22:10 +0200

    use '/' to search

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,17 +1,24 @@
 <form action="/">
-    <div>
+    <div style="display: flex; align-items: center;">
         <input
             type="text"
             name="search"
+            id="searchInput"
             style="margin-left:10px"
             placeholder="Search Desired Car..."
             class="border-2 border-black rounded"
         />
-
-            <button type="submit">
-                <i class="fa fa-search"></i>
-                Search
-            </button>
-
+        <div style="display: flex; align-items: center; justify-content: center; width: 22px; height: 22px; border: 1px solid #c8ccd0; border-radius: 3px; margin-left: 5px;">
+            <div style="color: #586069; font-size: 12px;">/</div>
+        </div>
     </div>
 </form>
+
+<script>
+    document.addEventListener('keydown', function(event) {
+        if (event.key === '/') {
+            event.preventDefault();
+            document.getElementById('searchInput').focus();
+        }
+    });
+</script>
